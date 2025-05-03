041bb625d3f87d5f1f9eeb59322de8a8cc46d240
PaceGG <yura3012004@gmail.com>
2025-05-03 22:27:00 +0500

    начальный layout без стилей

---
+++ src/navigation/index.tsx
@@ -0,0 +1,55 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+import HomeScreen from "../screens/HomeScreen";
+import TasksScreen from "../screens/TasksScreen";
+import CalendarScreen from "../screens/CalendarScreen";
+import ChatsScreen from "../screens/ChatsScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: any;
+
+            switch (route.name) {
+              case "Home":
+                iconName = "home";
+                break;
+              case "Tasks":
+                iconName = "task";
+                break;
+              case "Calendar":
+                iconName = "calendar-month";
+                break;
+              case "Chats":
+                iconName = "chat";
+                break;
+              case "Profile":
+                iconName = "person";
+                break;
+            }
+
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "black",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Tasks" component={TasksScreen} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="Chats" component={ChatsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
