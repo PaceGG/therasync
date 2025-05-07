c309240a1700c1d9df8d2735bd5a8d84e079d193
PaceGG <yura3012004@gmail.com>
2025-05-07 22:22:26 +0500

    изменен компонент CustomButton

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -3,17 +3,26 @@ import { Text, TouchableOpacity } from "react-native";
 type Props = {
   title: string;
   backgroundColorProp: string;
+  onClick: () => void;
 };
 
 export default function CustomButton({
   title,
   backgroundColorProp: backgroundColorProp,
+  onClick,
 }: Props) {
   return (
     <TouchableOpacity
-      style={{ backgroundColor: backgroundColorProp, elevation: 3 }}
+      style={{
+        backgroundColor: backgroundColorProp,
+        elevation: 3,
+        borderRadius: 10,
+      }}
+      onPress={onClick}
     >
-      <Text>{title}</Text>
+      <Text style={{ fontSize: 20, textAlign: "center", padding: 8 }}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
