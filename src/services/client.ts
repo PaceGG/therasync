814c59e86012ddc038fb2217783668b765a9774a
PaceGG <yura3012004@gmail.com>
2025-05-22 19:30:03 +0500

    список клиентов в профиле

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,12 @@
-import { Client } from '../types/client';
-import { mockClients } from './mockData';
+import { Client } from "../types/client";
+import { mockClients } from "./mockData";
 
 export const getClientsByPsychologist = async (): Promise<Client[]> => {
   return mockClients;
 };
 
 export const searchClients = async (name: string): Promise<Client[]> => {
-  return mockClients.filter(c => c.firstName.toLowerCase().includes(name.toLowerCase()));
+  return mockClients.filter((c) =>
+    c.firstName.toLowerCase().includes(name.toLowerCase())
+  );
 };
