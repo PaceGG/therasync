0124ade0b56c36c65f32e58233b0d4fb08880823
PaceGG <yura3012004@gmail.com>
2025-05-24 02:52:52 +0500

    создание чат-комнаты после добавления пользователя

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,5 +1,7 @@
 import { Client } from "../types/client";
 import { mockClients } from "./mockData";
+import { createChatRoom } from "./chat";
+import { getUserId } from "./auth";
 
 export const getClientsByPsychologist = async (): Promise<Client[]> => {
   return mockClients;
@@ -9,6 +11,8 @@ export const addClient = async (client: Client) => {
   const exists = mockClients.some((c) => c.id === client.id);
   if (exists) throw new Error("CLIENT_IS_EXISTS");
   mockClients.push(client);
+  const userId = await getUserId();
+  createChatRoom(client.id, userId);
 };
 
 export const deleteClient = async (id: number): Promise<void> => {
